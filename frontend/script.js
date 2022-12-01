903579878d61b5d73560cae6a608a3042cbd8184
ronaldof16 <ronaldof16@hotmail.com>
2022-11-30 23:30:06 -0300

    Concluindo a validação do formulário fale conosco das páginas lde login e de adicionar produto.

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,7 @@ let form = document.querySelector('form');
 let campos = document.querySelectorAll('.texto');
 let nome = document.querySelector('#nome');
 let msg = document.querySelectorAll('.alerta');
+let form2 = document.querySelector('#form2');
 
 function setError(index) {
     campos[index].style.border = '2px solid red';
@@ -30,6 +31,12 @@ function validarMensagem() {
 }
 
 form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validarNome();
+    validarMensagem();
+});
+
+form2.addEventListener('submit', (event) => {
     event.preventDefault();
     validarNome();
     validarMensagem();
